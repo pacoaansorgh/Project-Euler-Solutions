24d1b16c6383df0fdfbfdfcc28eb323eb4895115
Paco Aansorgh <pacoaansorgh@hotmail.com>
2018-06-04 18:48:35 +0200

    Improved solution for problem 024
    Removed unnecessary operations pre-calculating factorial numbers. Added a factorial function. Removed unnecessary if-statement.

--- Problem 024 - Lexicographic permutations.cpp
+++ Problem 024 - Lexicographic permutations.cpp
@@ -12,33 +12,27 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 #include <algorithm>
 #define TARGET 1000000
 #define DIGITS 10
-// temp target = 2301
 
 using namespace std;
 
+// Return the factorial of given integer.
+int factorial(int n) {
+	if (n > 1) { return n * factorial(n - 1); }
+	else { return 1; }
+}
+
 int main()
 {
-	// Calculate number of repetitions a digit has before it changes. (repetitions equals digit * previous digit's repetitions)
-	int repetitions[DIGITS] = { 1 };
-	for (int i = 1; i < DIGITS; i++) {
-		repetitions[i] = i * repetitions[i - 1];
-	}
-	for (int i = DIGITS - 1; i >= 0; i--) {
-		cout << "Number of repetitions for digit " << DIGITS - (i + 1) << " is: " << repetitions[i] << endl;
-	}
-	cout << endl;
-
 	// Calculate the number of iterations (times a digit changes) each digit has in our target amount of permutations
 	int iteration = TARGET - 1;
 	int count = 1;
 	int iterations[DIGITS] = { 0 };
 	while (count <= DIGITS) {
-		int divisions = iteration / repetitions[DIGITS - count];
-		if (divisions >= 0) {
-			cout << "Number of iterations for digit " << count - 1 << " is: " << divisions << endl;
-			iterations[count - 1] = divisions;
-		}
-		iteration %= repetitions[DIGITS - count];
+		int fact = factorial(DIGITS - count);
+		int divisions = iteration / fact;
+		cout << "Number of iterations for digit " << count - 1 << " is: " << divisions << endl;
+		iterations[count - 1] = divisions;
+		iteration %= fact;
 		count++;
 	}
 	cout << endl;
@@ -51,7 +45,6 @@ int main()
 			}
 		}
 	}
-	cout << endl;
 
 	// Now print the final values:
 	cout << "The " << TARGET << "th permutation of  these " << DIGITS << " digits is: ";
