77a0c06ca22097df61b9d351c74f65e500822dc4
Paco Aansorgh <pacoaansorgh@hotmail.com>
2018-09-18 11:59:49 +0200

    Added notes on optimization to problem 005
    Noted specific optimizations that could be done since we know our specific target number. Made a small optimization by looking at the divisors from high to low, rather than low-high. Added another small optimization of skipping odd numbers, since they will never be divisible by even numbers.

--- Problem 005 - Smallest multiple.cpp
+++ Problem 005 - Smallest multiple.cpp
@@ -1,17 +1,21 @@
-// Smallest number evenly divisible by all numbers from 1 to 20
+/* 
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
 #include <iostream>
 #define DIVISORS 20
 
 using namespace std;
 
 int main(){
-    int num = 1;
+    int num = 2;
     // This variable could be cut to save some memory, but it improves code clarity.
     bool found = false;
     while (!found) {
         bool divisible = true;
         // Check the numer against all divisors. If we find one that fails, go to the next number.
-        for (int i = 1; i <= DIVISORS; i++) {
+        for (int i = DIVISORS; i >= 2; i--) {
             if (num % i != 0) {
                 divisible = false;
                 break;
@@ -21,8 +25,20 @@ int main(){
         if (divisible) {
             break;
         }
-        num += 1;   
+        num += 2;   
     }
     cout<<"Smallest number evenly divisible by 1 through "<<DIVISORS<<" is: "<<num;
-	return 0;
+    return 0;
 }
+
+/*
+Some notes on optimization:
+We could speed up execution a lot by skipping over unnecessary numbers. However, this would limit the flexibility of the program.
+With the specific target divisors up to 20, we could:
+start looking at 2640, the first number after 2520 that is divisible by both 11 and 20.
+increment in steps of 220 rather than 2. This way we skip all numbers not divisible by both 11 and 20.
+We could stop checking divisors below 11, since we've checked their doubles already.
+
+All of those would speed up the program a lot, but would require specific adaptation to the target divisor limit of 20.
+For an example of the optimized, targetted version of this, look at the LUA code sample of this same problem.
+*/
